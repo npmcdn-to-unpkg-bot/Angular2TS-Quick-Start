84b6f3e027249b8f29746d150d8619f2983d81f6
Shyam Chen <shyamchen1994@gmail.com>
2016-07-02 16:56:50 +0800

    52nd Commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app',
+  template: `
+    <h3 class="title">Angular2TS Quick Start</h3>
+    <div class="content">
+      <p>Hello Angular 2</p>
+    </div>
+  `,
+  styles: [`
+    .title { color: #3F51B5; text-align: center; }
+    .content { display: flex; justify-content: center; }
+  `],
+  directives: [],
+  providers: [],
+  viewProviders: [],
+  pipes: []
+})
+export class AppComponent implements OnInit {
+  ngOnInit(): void {
+    console.log('Hello Angular 2')
+  }
+}
--- src/app/main.ts
+++ src/app/main.ts
@@ -1,10 +1,5 @@
 import { bootstrap } from '@angular/platform-browser-dynamic';
-import { ROUTER_PROVIDERS } from '@angular/router';
-import { HTTP_PROVIDERS} from '@angular/http';
 
-import { App } from './app';
+import { AppComponent } from './app.component';
 
-bootstrap(App, [
-  ROUTER_PROVIDERS,
-  HTTP_PROVIDERS
-]);
+bootstrap(AppComponent, []);
