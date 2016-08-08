5756fa80d6493c294fece1b8a7c7b6135aee27b9
Shyam Chen <shyamchen1994@gmail.com>
2016-08-08 14:49:56 +0800

    61st Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,19 +4,10 @@ import { Component, OnInit } from '@angular/core';
   selector: 'app',
   template: `
     <h3 class="title">Angular2TS Quick Start</h3>
-    <div class="content">
-      <p>Hello Angular 2</p>
-    </div>
   `,
   styles: [`
     .title {
       color: #3F51B5;
-      text-align: center;
-    }
-
-    .content {
-      display: flex;
-      justify-content: center;
     }
   `],
   directives: [],
