bb945ed43c94250a123e84f118e00f20da44b508
Shyam Chen <shyamchen1994@gmail.com>
2016-08-10 14:04:38 +0800

    64th Commit

--- src/system.config.ts
+++ src/system.config.ts
@@ -9,7 +9,7 @@ const map: any = {
   '@angular/forms': `https://npmcdn.com/@angular/forms${formsVer}`,
   'angular2-in-memory-web-api': 'https://npmcdn.com/angular2-in-memory-web-api',
   'rxjs': 'https://npmcdn.com/rxjs@5.0.0-beta.6',
-  'ts': 'https://npmcdn.com/plugin-typescript@4.0.10/lib/plugin.js',
+  'ts': 'https://npmcdn.com/plugin-typescript@4.0.12/lib/plugin.js',
   'typescript': 'https://npmcdn.com/typescript@1.8.10/lib/typescript.js'
 };
 
